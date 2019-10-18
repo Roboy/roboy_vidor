e1b3a2e9d066b2fa769d981b4658067e851dc8a8
letrend <st@gi.ai>
2019-10-19 01:38:42 +0200

    better

--- projects/MKRVIDOR4000_template/output_files/VidorFPGARecovery/resetVidorFPGA.bash
+++ projects/MKRVIDOR4000_template/output_files/VidorFPGARecovery/resetVidorFPGA.bash
@@ -4,9 +4,9 @@ sleep 4
 echo "wait..."
 ./tools/linux/clujtag -p /dev/$1 -s boot.svf
 BACK_PID=$!
-sleep 20
+sleep 1
 read -r -p "Double-tap reset button quickly, then hit enter" response
 if [[ $response =~ ^()$ ]]; then
-	    ./tools/linux/bossac -i -d --port=/dev/$1 -I -U true -i -e -w -v RestoreFPGABootloader.ino.bin -R
+	    ./tools/linux/bossac -i -d --port=/dev/$1 -I -U true -i -e -w RestoreFPGABootloader.ino.bin -R
 fi
 echo "done"
